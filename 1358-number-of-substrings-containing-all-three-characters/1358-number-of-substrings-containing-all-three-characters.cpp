3db3327ecd16189c1817c039b7a4a083d34a1519
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-03-20 14:55:22 +0530

    Time: 1718 ms (5.08%), Space: 11.2 MB (31.64%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int l =0,r=0;
+        unordered_map<char,int> mp;
+        int n = s.size();
+        int res = 0;
+        mp['a'] = 0;mp['b'] = 0;mp['c'] = 0;
+        mp[s[r]]++;
+        while( r<s.length()){
+            while(r<n && (mp['a'] < 1 || mp['b'] < 1 || mp['c'] < 1) ){  
+                r++;    
+                mp[s[r]]++;            
+            }
+            cout<<r<<'&'<<l<<' ';
+
+            res += n-r;
+            mp[s[l]]--;
+            l++;
+        }
+        return res;
+    }
+};
