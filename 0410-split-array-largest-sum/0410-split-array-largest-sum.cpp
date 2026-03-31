8097ac655a55332418f4c8db427c9afeebdd6f7b
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-03-31 20:14:31 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.32%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& nums, int k, int mid){
-        int load = 0;
+    bool isPossible(vector<int>& nums, int k , int mid){
         int curr = 1;
+        int currsum = 0;
         for(auto x: nums){
-            if(load+ x <= mid){
-                load+=x;
-            }else{
-                load = x;
+            if(currsum+x > mid){
+                currsum = x;
                 curr++;
+            }else{
+                currsum += x;
             }
         }
-        return (curr <= k)? true :false;
+        return (curr <= k)? true: false;
     }
     int splitArray(vector<int>& nums, int k) {
         int low = *max_element(nums.begin(),nums.end());
         int high = accumulate(nums.begin(),nums.end(),0LL);
-
-        int ans = 0;
-        while( low <= high){
+        int ans = -1;
+        while(low <= high){
             int mid = low + (high - low)/2;
             if(isPossible(nums,k,mid)){
                 ans = mid;
                 high = mid-1;
-            }else low = mid +1;
+            }else{
+                low = mid +1;
+            }
         }
         return ans;
     }
