21f12fb7048e66d81ccca41a4ea6c9f797cf48f6
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-04-03 07:32:23 +0530

    Time: 90 ms (35.53%), Space: 56 MB (74.08%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int atmost(vector<int>& nums, int k){
-        int l = 0, r = 0;
-        int res = 0;
+        int l = 0 , r = 0;
         unordered_map<int,int> mp;
-        while( l <= r && r<nums.size()){
+        int res = 0;
+        while(r<nums.size()){
             mp[nums[r]]++;
             while(mp.size() > k){
                 mp[nums[l]]--;
