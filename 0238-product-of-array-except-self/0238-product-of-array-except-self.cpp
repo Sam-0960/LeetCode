aa54da6e18b9df047b0f5d4b3b07c590f4b2777b
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-04-04 09:33:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (31.43%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,19 +4,16 @@ public:
         int n = nums.size();
         vector<int> pref(n);
         vector<int> suff(n);
-
-        pref[0] = nums[0];
-        for(int i = 1; i <nums.size(); i++) pref[i] = nums[i] * pref[i-1];
-        suff[n-1] = nums[n-1];
-        for(int i=n-2; i>= 0 ; i--) suff[i] = suff[i+1] * nums[i];
-
         vector<int> ans(n);
-        ans[0] = suff[1];
-        ans[n-1] = pref[n-2];
 
-        for(int i = 1 ; i<nums.size()-1 ; i++){
-            ans[i] = pref[i-1]*suff[i+1];
-        }
+        pref[0] = 1;
+        for(auto i = 1; i<nums.size(); i++) pref[i] = pref[i-1]*nums[i-1];
+
+        suff[n-1] = 1;
+        for(int i= n-2 ; i>= 0 ; i--) suff[i] = suff[i+1]*nums[i+1];
+
+        for(int i=0 ; i<nums.size(); i++) ans[i] = pref[i]*suff[i];
+
         return ans;
 
     }
