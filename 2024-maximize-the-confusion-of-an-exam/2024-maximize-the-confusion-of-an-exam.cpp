75843e10f669dca31736b0c853f81e93f99260fa
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-04-09 17:14:04 +0530

    Time: 6 ms (73.89%), Space: 14.5 MB (7.62%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int maxTrue(string arr, int k){
+    int maxlen(string arr , int k , int c){
+        int cnt = 0;
+        int l = 0 ; 
+        int r = 0;
         int maxlen = 0;
-        int count = 0;
-        int l=0,r=0;
-        while( l<= r && r<arr.length()){
-            if(arr[r] == 'F'){
-                if(count < k){
-                    count++;
-                }else{
-                    while(arr[l] == 'T') l++;
+        while(r<arr.size()){
+            if(arr[r] != c) cnt++;
+            if(cnt > k){
+                while(cnt != k){
+                    if(arr[l] != c) cnt--;
                     l++;
                 }
             }
@@ -18,25 +18,7 @@ public:
         }
         return maxlen;
     }
-    int maxFalse(string arr, int k){
-        int maxlen = 0;
-        int count = 0;
-        int l =0,r=0;
-        while( l<=r && r<arr.size()){
-            if(arr[r] == 'T'){
-                if(count < k){
-                    count++;
-                }else{
-                    while(arr[l] == 'F') l++;
-                    l++;
-                }
-            }
-            maxlen = max(maxlen , r-l+1);
-            r++;
-        }
-        return maxlen;
-    }
     int maxConsecutiveAnswers(string answerKey, int k) {
-        return max(maxTrue(answerKey,k),maxFalse(answerKey,k));
+        return max(maxlen(answerKey,k ,'T'),maxlen(answerKey,k ,'F'));
     }
 };
