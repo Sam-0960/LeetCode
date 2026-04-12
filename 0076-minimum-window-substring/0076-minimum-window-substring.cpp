9c50dc6c3841a9cec29d0606dcc7270b015755c5
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-04-12 09:48:27 +0530

    Time: 5 ms (64.48%), Space: 11.5 MB (41.28%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,28 +3,27 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char,int> mp;
         for(auto x: t) mp[x]++;
-
-        int count = 0;
-        int n = mp.size();
-        int l = 0 ,r = 0;
+        int l = 0 , r = 0;
         int minlen = INT_MAX;
-        int sindex = -1;
-        while(r<s.size()){
+        int start = -1;
+        // move right ptr until all the els in t fit inside my window in s
+        // move left ptr to only optimise the window len to minimum
+        int n = mp.size();
+        int cnt = 0;
+        while(r < s.size()){
             mp[s[r]]--;
-            if(mp[s[r]] == 0) count++;
-            while( count == n){
-                if(minlen > r-l+1){
-                        minlen = r-l+1;
-                        sindex = l;
+            if(mp[s[r]] == 0) cnt++;
+            while(cnt == n){
+                if( r-l+1 < minlen){
+                    start = l;
+                    minlen = r-l+1;
                 }
                 mp[s[l]]++;
-                if(mp[s[l]] > 0){
-                    count--;
-                }
+                if(mp[s[l]] > 0) cnt--;
                 l++;
             }
             r++;
         }
-        return (sindex == -1)? "" : s.substr(sindex,minlen);
+        return (start == -1)? ""  : s.substr(start,minlen);
     }
 };
