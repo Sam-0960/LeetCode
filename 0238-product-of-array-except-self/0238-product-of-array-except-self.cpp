2f7fdad02cc5509b6ad0eafaa2ccfcf41a050f43
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-05-27 10:20:26 +0530

    Time: 0 ms (100%), Space: 42.1 MB (31.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefix(nums.size());
-        vector<int> suffix(nums.size());
-        prefix[0] = nums[0];
-        cout << prefix[0] <<endl;
-        for(int i = 1 ; i< nums.size();i++){
-            prefix[i] = nums[i] * prefix[i-1];
-            cout << prefix[i] <<endl;
-        }
-        suffix[nums.size()-1] = nums[nums.size()-1];
-        cout << suffix[nums.size()-1] << endl;
-        for(int i = nums.size()-2; i>=0; i--){
-            suffix[i] = suffix[i+1]*nums[i];
-            cout << suffix[i] <<endl;
-        }
+        int n = nums.size();
+        vector<int> pref(n);
+        vector<int> suff(n);
+        vector<int> ans(n);
 
-        vector<int> ans(nums.size());
-        ans[0] = suffix[1];
-        ans[nums.size()-1] = prefix[nums.size()-2];
-        for(int i=1; i<=nums.size()-2; i++){
-            ans[i] = prefix[i-1] * suffix[i+1];
-        }
+        pref[0] = 1;
+        for(auto i = 1; i<nums.size(); i++) pref[i] = pref[i-1]*nums[i-1];
+
+        suff[n-1] = 1;
+        for(int i= n-2 ; i>= 0 ; i--) suff[i] = suff[i+1]*nums[i+1];
+
+        for(int i=0 ; i<nums.size(); i++) ans[i] = pref[i]*suff[i];
+
         return ans;
+
     }
 };
