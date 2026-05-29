13aed7ffbe78adc157cf2aa2c384a130f1c5ac30
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-05-29 13:09:44 +0530

    Time: 15 ms (18.87%), Space: 13.8 MB (5.74%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int maxletter(string s,int k, char x){
+    int atmost(string s , int k , char x){
+        int l = 0 ,r = 0;
+        int maxlen = 0;
         int count = 0;
-        int l=0,r=0;
-        int maxlen= 0;
-        while(l<=r && r<s.length()){
+        while(r<s.size()){
             if(s[r] != x) count++;
+
             while(count > k){
                 if(s[l] != x) count--;
                 l++;
             }
-            
-            maxlen = max(maxlen,r-l+1);
+            maxlen = max(maxlen , r-l+1);
             r++;
         }
         return maxlen;
     }
+
     int characterReplacement(string s, int k) {
-        unordered_map<char,int> v;
-        for(auto x: s) v[x]++;
-        int res = 0;
-        for(auto it:v){
-            res = max(res,maxletter(s,k,it.first));
+        unordered_set<int>st (s.begin(),s.end());
+        int maxlen = 0;
+        for(auto x: st){
+            maxlen = max(maxlen , atmost(s,k,x));
         }
-        return res;
+        return maxlen;
     }
 };
