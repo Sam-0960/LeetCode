0e62622cd9328a42ab6747692584fb52f748b869
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-04 07:50:04 +0530

    Time: 3 ms (65.8%), Space: 10.1 MB (46.2%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         unordered_map<char,int> mp;
-        int l = 0 , r = 0;
-        int cnt = s1.size();
         for(auto x: s1) mp[x]++;
-        while(r<s2.size()){
+        int cnt = s1.size();
+        int l = 0 , r = 0;
+        while(r < s2.size()){
             mp[s2[r]]--;
-            if(mp[s2[r]] >= 0) cnt--;
-            while(cnt == 0){
+            if(mp[s2[r]]>=0) cnt--;
+            while(cnt==0){
                 if(r-l+1 == s1.size()) return true;
                 mp[s2[l]]++;
                 if(mp[s2[l]] > 0) cnt++;
