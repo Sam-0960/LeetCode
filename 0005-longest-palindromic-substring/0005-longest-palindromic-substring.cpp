349e9d9b51dd0298cb2ebfa67a6c4ceec9b455c5
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-11 10:34:08 +0530

    Time: 79 ms (40.97%), Space: 169.5 MB (17.57%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    pair<int,int> checkPal(string s, int l , int r){
-        while(l>=0 && r < s.size() && s[l] == s[r]){
-            l--; 
+    pair<int,int> check(string s, int l, int  r){
+        while(l >= 0 && r < s.size() && s[l] == s[r]){
+            l--;
             r++;
         }
         return {l+1,r-l-1};
@@ -10,13 +10,19 @@ public:
 
     string longestPalindrome(string s) {
         int maxlen = 0;
-        int start = 0;
-        for(int i=0 ; i<s.size(); i++){
-            auto [s1,l1] = checkPal(s,i,i);
-            auto [s2,l2] = checkPal(s,i,i+1);
-            if (l1 > maxlen) { maxlen = l1; start = s1; }
-            if (l2 > maxlen) { maxlen = l2; start = s2; }
+        string res = "";
+        for(int i = 0 ; i <s.size() ; ++i){
+            pair<int,int> even = (i+1 < s.size()) ? check(s,i,i+1) : pair<int,int>{0,0} ;
+            pair<int,int> odd = check(s, i, i);
+            if(odd.second > maxlen){
+                res = s.substr(odd.first,odd.second); 
+                maxlen = odd.second;
+            }
+            if(even.second > maxlen){
+                res = s.substr(even.first,even.second);
+                maxlen = even.second;
+            } 
         }
-        return s.substr(start,maxlen);
+        return res;
     }
 };
