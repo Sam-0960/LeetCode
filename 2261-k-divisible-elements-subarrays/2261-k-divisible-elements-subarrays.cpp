a878d795ffcd1dda6d17524c593d18ab62464f92
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-13 11:39:30 +0530

    Time: 871 ms (59.55%), Space: 236.1 MB (36.94%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    struct VectorHash {
-    size_t operator()(const vector<int>& v) const {
-        size_t hash = 0;
-        for (int x : v) {
-            hash ^= hash * 31 + x;
-        }
-        return hash;
-        }
-    };
     int countDistinct(vector<int>& nums, int k, int p) {
-        unordered_set<vector<int>,VectorHash> st;
-        for(int i =0 ; i<nums.size();i++){
+        set<vector<int>> st;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
             int cnt = 0;
-            for(int j = i;j<nums.size(); j++){
-                    if(nums[j]%p == 0)cnt++;
-                    if(cnt > k) break;
-                    vector<int> v(nums.begin()+i,nums.begin()+j+1);
-                    st.insert(v);                
+            vector<int> cur;
+
+            for(int j=i;j<n;j++){
+                cur.push_back(nums[j]);
+
+                if(nums[j] % p == 0)
+                    cnt++;
+
+                if(cnt > k)
+                    break;
+
+                st.insert(cur);
             }
         }
+        
         return st.size();
     }
 };
