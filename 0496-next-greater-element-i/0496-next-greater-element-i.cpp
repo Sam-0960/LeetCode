33d9568480e158578062543b0c4c4a34a7c8fd9b
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-23 20:28:31 +0530

    Time: 0 ms (100%), Space: 12.9 MB (38.38%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        for(int i = 0 ; i<nums1.size() ; i++){
-            int el = nums1[i];
-            for(int j = 0 ; j<nums2.size(); j++){
-                int flag = 0;
-                if(j == nums2.size()-1 && nums2[j] == el){ ans.push_back(-1); continue;}
-                if(nums2[j] == el){
-                    flag  = 0;
-                    for(int k = j+1; k<nums2.size() ; k++){
-                        if(nums2[k] > el){
-                            ans.push_back(nums2[k]);flag  = 1; break;
-                        }
-                        if(k == nums2.size()-1 && flag == 0) ans.push_back(-1);
-                    }
-                }
-                if(flag == 1) break;
+        vector<int>ans;
+        stack<int> st;
+        for(int i = nums2.size()-1 ; i>=0; i--){
+            if(st.empty()) ans.push_back(-1);
+            else if(st.top() > nums2[i]) ans.push_back(st.top());
+            else{
+                while(!st.empty() && st.top() <= nums2[i]) st.pop();
+                if(st.empty()) ans.push_back(-1);
+                else ans.push_back(st.top());
             }
+            st.push(nums2[i]);
         }
-        return ans;
+        unordered_map<int,int> mp;
+        for(int i = 0 ; i<nums2.size();i++){
+            mp[nums2[i]] = ans[nums2.size()-1-i];
+        }
+        vector<int> res;
+        for(auto x: nums1) res.push_back(mp[x]);
+        return res;
     }
 };
