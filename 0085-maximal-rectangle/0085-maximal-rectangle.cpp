83e60890fd69d5e93ca2dd9308ed0f8ae3ca1706
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-28 15:03:18 +0530

    Time: 8 ms (55.04%), Space: 19.7 MB (54.95%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    long long max_rect_area(vector<int>& arr){
-        long long maxi = 0;
+    int area(vector<int>& arr){
         stack<int> st;
-        for(int i = 0; i <arr.size(); i++){
+        int n = arr.size();
+        int maxi = 0;
+        for(int i = 0 ; i<n; i++){
             while(!st.empty() && arr[st.top()] > arr[i]){
-                long long el = arr[st.top()];
+                int el = arr[st.top()];
                 st.pop();
-                long long nse = i;
-                long long pse = (st.empty())? -1:st.top();
-                maxi = max(maxi,(el*(nse-pse-1)));
+                int nse = i;
+                int pse = (st.empty())? -1: st.top();
+                maxi = max(maxi, el*(nse-pse-1));
             }
             st.push(i);
         }
         while(!st.empty()){
-            long long el = arr[st.top()];
+            int el = arr[st.top()];
             st.pop();
-            long long nse = arr.size();
-            long long pse = (st.empty())? -1:st.top();
-            maxi = max(maxi,(el*(nse-pse-1)));
+            int nse = n;
+            int pse = (st.empty())? -1: st.top();
+            maxi = max(maxi, el*(nse-pse-1));
         }
         return maxi;
     }
-
     int maximalRectangle(vector<vector<char>>& matrix) {
-        long long maxarea = 0;
-        int rows = matrix.size(), cols = matrix[0].size();
-        vector<int> hist(cols, 0);
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                hist[j] = (matrix[i][j] == '1') ? hist[j] + 1 : 0;
+        int maxArea = 0;
+        int rows = matrix.size(); int cols = matrix[0].size();
+        vector<int> histogram(cols,0);
+        for(int i = 0 ; i<rows; i++){
+            for(int j=0;j<cols; j++){
+                histogram[j] = (matrix[i][j] == '1')? histogram[j]+1 : 0;
             }
-            maxarea = max(maxarea, max_rect_area(hist));
+            maxArea = max(maxArea , area(histogram));
         }
-        return (int)maxarea;
+        return maxArea;
     }
-
 };
