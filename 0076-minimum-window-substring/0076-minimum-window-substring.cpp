a5a01d04c9545ceb9c8f22818cb2709dda3a9197
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-29 21:59:32 +0530

    Time: 2 ms (80.91%), Space: 11.5 MB (64.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -4,23 +4,25 @@ public:
         unordered_map<char,int> mp;
         for(auto x: t) mp[x]++;
         int l = 0 ; int r = 0;
-        int len = INT_MAX;
-        int cnt = t.size();
+        int minlen = INT_MAX;
         int start = -1;
+        int k = mp.size();
+        int cnt = 0;
         while(r<s.size()){
             mp[s[r]]--;
-            if(mp[s[r]] >= 0) cnt--;
-            while(cnt == 0){
-                if(r-l+1 < len){ 
+            if(mp[s[r]] == 0) cnt++;
+            while(cnt == k){
+                int len = r-l+1;
+                if(len < minlen){
                     start = l;
-                    len = r-l+1;
+                    minlen = len;
                 }
                 mp[s[l]]++;
-                if(mp[s[l]] > 0) cnt++;
+                if(mp[s[l]] > 0) cnt--;
                 l++;
             }
             r++;
         }
-        return (start == -1)? "": s.substr(start,len);
-    }   
+        return (start == -1)? "":s.substr(start,minlen);
+    }
 };
