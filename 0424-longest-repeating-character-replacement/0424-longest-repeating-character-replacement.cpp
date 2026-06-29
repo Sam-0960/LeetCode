fdd7dd18b1071d11f0da83062b099445b8e47c57
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-06-30 00:39:10 +0530

    Time: 25 ms (10.28%), Space: 23.1 MB (5.81%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int atmost(string s , int k , char x){
-        int l = 0 ,r = 0;
-        int maxlen = 0;
+    int func(string s, int k, char x){
         int count = 0;
+        int l = 0 ; int r = 0;
+        int maxlen = 0;
         while(r<s.size()){
             if(s[r] != x) count++;
-
             while(count > k){
                 if(s[l] != x) count--;
                 l++;
             }
-            maxlen = max(maxlen , r-l+1);
+            maxlen = max(maxlen,r-l+1);
             r++;
         }
         return maxlen;
     }
-
     int characterReplacement(string s, int k) {
         int maxlen = 0;
-        for(int i=0; i<26;i++){
-            maxlen = max(maxlen , atmost(s,k,(char)(i+65)));
+        for(int i = 0 ; i<26; i++){
+            maxlen = max(maxlen, func(s,k,(char)('A'+i)));
         }
         return maxlen;
     }
