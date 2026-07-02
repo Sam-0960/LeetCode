c353a2a75a2f65a884b8f7f17eb7b2e046b370a6
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-02 11:58:26 +0530

    Time: 6 ms (93.51%), Space: 35.2 MB (19.38%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool probable(vector<int>& weights, int days, int mid){
-        int cnt = 1;
-        int load = 0;
-        for(int i= 0; i<weights.size(); i++){
-            if( load + weights[i] <= mid){
-                load += weights[i];
-            }else{ 
-                load = weights[i];
-                cnt++;
+    bool possible(vector<int>& weights, int days, long long mid){
+        long long load = 0;
+        long long Days = 1;
+        for(auto x: weights){
+            if(load + x > mid){
+                load = x;
+                Days++;
+            }else{
+                load += x;
             }
-            if(cnt > days) return false;    
         }
-        return (cnt <= days);
+        return (Days <= days);
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int right = accumulate(weights.begin(),weights.end(),0);
-        int left = *max_element(weights.begin(),weights.end());
-        int ans = 0;
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            if( probable(weights, days, mid)){
-                right = mid-1;
+        long long max = accumulate(weights.begin(),weights.end(),0LL);
+        long long min = *max_element(weights.begin(),weights.end());
+        long long ans = min;
+        while(min <= max){
+            long long mid = min + (max-min)/2;
+            if(possible(weights,days,mid)){
                 ans = mid;
+                max = mid-1;
             }else{
-                left = mid+1;
+                min = mid+1;
             }
         }
         return ans;
