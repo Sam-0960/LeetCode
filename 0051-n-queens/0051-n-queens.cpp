b73feb6efb6ba0753608a92b136607d6ace31a8e
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-05 21:14:59 +0530

    Time: 0 ms (100%), Space: 10.7 MB (30.61%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    bool isSafe(int row, int col,vector<string>& board,int n){
-        int duprow = row;
-        int dupcol = col;
-        while(duprow >= 0){
-            if(board[duprow--][dupcol] == 'Q') return false;
-        }
-        duprow = row;
-        dupcol = col;
-        while(duprow >= 0 && dupcol >= 0){
-            if(board[duprow--][dupcol--] == 'Q') return false;
-        }
-        duprow = row;
-        dupcol = col;
-        while(duprow >= 0 && dupcol < n){
-            if(board[duprow--][dupcol++] == 'Q') return false;
-        }
-        return true;
-    }
-    void solve(int row, vector<string>& board, vector<vector<string>>& ans,int n ){
+    vector<vector<string>> ans;
+
+    void func(int row, vector<string>& board, int n,vector<int>& upper_left,vector<int>& upper_right,vector<int>& horizontal){
         if(row == n){
             ans.push_back(board);
             return;
         }
-        for(int i = 0 ; i<n; i++){
-            if(isSafe(row, i, board, n)){
-                board[row][i] = 'Q';
-                solve(row+1,board, ans,n);
-                board[row][i] = '.';
+        for(int col = 0 ; col<n ;col++){
+            if(upper_left[row+col] == 0 && upper_right[n-1+row-col] == 0 && horizontal[col] == 0){
+                upper_left[row+col] = 1;
+                upper_right[n-1+row-col] = 1;
+                horizontal[col] = 1;
+                board[row][col] = 'Q';
+                func(row+1,board,n,upper_left,upper_right,horizontal);
+                board[row][col] = '.';
+                upper_left[row+col] = 0;
+                upper_right[n-1+row-col] = 0;
+                horizontal[col] = 0;
             }
         }
+
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        string v;
-        for(int i=0; i<n; i++) v.push_back('.');
+        string s;
+        for(int i = 0 ; i < n ; i++) s.push_back('.');
         vector<string> board(n);
+        for(int i=0;i<n;i++) board[i] = s;
 
-        for(int i=0 ; i<n; i++){
-            board[i]= v;
-        }
+        vector<int> upper_right(2*n-1);
+        vector<int> upper_left(2*n-1);
+        vector<int> horizontal(n);
+        func(0,board, n, upper_right,upper_left,horizontal);
 
-        solve(0,board, ans, n);
         return ans;
     }
 };
