e493301fbab026d0c2dc7d0193654f366c1d0da3
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-06 11:51:35 +0530

    Time: 832 ms (20.23%), Space: 11.6 MB (30.13%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    bool search_from_here(int row, int col,vector<vector<char>>& board, string word, int index){
-        if(index == word.size())  return true;
-        if(row < 0 || row == board.size() || col < 0 || col == board[0].size() || board[row][col] != word[index] ) return false;
-        char ch = board[row][col];
+    bool func(int row, int col,vector<vector<char>>& board, string word, int index){
+        if(index == word.size()) return true;
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[index])  return false;
+        char el = board[row][col];
         board[row][col] = '#';
-        bool op1 = search_from_here(row+1,col,board,word, index+1);
-        bool op2 = search_from_here(row,col+1,board,word, index+1);
-        bool op3 = search_from_here(row-1,col,board,word, index+1);
-        bool op4 = search_from_here(row,col-1,board,word, index+1);
-        board[row][col] = ch;
 
-        return op1 || op2 || op3 || op4;
+        //go up
+        bool up = func(row-1,col,board,word ,index+1);
+        //go down
+        bool down = func(row+1,col,board,word ,index+1);
+        //go left
+        bool left = func(row,col-1,board,word ,index+1);
+        // go right
+        bool right = func(row,col+1,board,word ,index+1);
+
+        board[row][col] = el;
+        return up||down||left||right;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        for(int i = 0; i<board.size(); i++){
-            for(int j = 0 ; j< board[0].size(); j++){
-                if(search_from_here(i,j,board,word,0)) return true;
+        bool ans = false;
+        for(int i=0 ; i<board.size(); i++){
+            for(int j = 0 ; j<board[0].size(); j++){
+                if(func(i,j,board,word,0)) return true;
             }
         }
-        return false;
+        return ans;
     }
 };
