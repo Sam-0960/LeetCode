fce4095e19894089d05421f54f559e693bc68b1c
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-07 10:07:50 +0530

    Time: 0 ms (100%), Space: 10.7 MB (50.86%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    vector<int> next_perm(vector<int> nums){
-        if(nums.size() == 0) return {};
-        if(nums.size() == 1) return {nums[0]};
-
+    vector<int> next_permut(vector<int>& nums){
         int dip = -1;
-        int n =nums.size();
-        for(int i=n-1; i>0;i--){
-            if(nums[i] > nums[i-1]){
+        for(int i = nums.size()-1; i>=1; i--){
+            if(nums[i-1] < nums[i]){
                 dip = i-1;
                 break;
             }
         }
-        if(dip == -1){ 
+        if(dip == -1){
             reverse(nums.begin(),nums.end());
             return nums;
         }
-        int el_ind = dip+1;
-        for(int i=dip+1; i<n;i++){
-            if(nums[i] > nums[dip] && nums[el_ind] >= nums[i] ) el_ind = i;
+            int index = nums.size()-1;
+        for(int i = nums.size()-1; i>= dip ;i--){
+            if(nums[dip] < nums[i]){
+                index = i;
+                break;
+            }
         }
-        swap(nums[dip], nums[el_ind]);
-        sort(nums.begin()+dip+1, nums.end());
+        swap(nums[index],nums[dip]);
+        reverse(nums.begin()+dip+1, nums.end());
         return nums;
     }
     int fact(int n){
         if(n == 0 || n == 1) return 1;
-        return n * fact(n-1);
+        return n*fact(n-1);
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int j = nums.size();    
-        int n = fact(j);
         vector<vector<int>> ans;
-    
-        for(int i = 0 ; i < n ; i++){
+        int n = fact(nums.size());
+        for(int i = 0; i <n; i++){
             ans.push_back(nums);
-            nums = next_perm(nums);
+            nums = next_permut(nums);
         }
         return ans;
     }
