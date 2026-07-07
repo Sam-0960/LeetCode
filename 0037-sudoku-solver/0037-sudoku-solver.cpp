269a93c11a39d51ec93aa1a7914422e38b97e50a
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-07 11:48:48 +0530

    Time: 477 ms (15.57%), Space: 8.7 MB (94.23%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    bool canPlace(vector<vector<char>>& board,int row, int col,int num){
-        for(int i = 0 ; i<board.size(); i++){
-            if(board[i][col] == num) return false;
-        }
-        for(int i = 0 ; i<board[0].size(); i++){
-            if(board[row][i] == num) return false;
-        }
-        
-        int grid_row = -1;
-        if(row>=0 && row <3) grid_row = 0;
-        else if(row>= 3 && row < 6) grid_row = 3;
-        else grid_row = 6;
-        int grid_col = -1;
-        if(col>=0 && col <3) grid_col = 0;
-        else if(col>= 3 && col < 6) grid_col = 3;
-        else grid_col = 6;
-
-        for(int i = grid_row ; i<3+grid_row; i++)
-            for(int j = grid_col; j<3+grid_col;j++)
-                if(board[i][j] == num)  return false;
+    bool caniplace(vector<vector<char>>& board, int row, int col, int num){
+        for(int i = 0 ; i< board.size() ; i++)  if(board[row][i] == num) return false;
+        for(int i = 0; i<board.size(); i++) if(board[i][col] == num) return false;
+        int r = row;
+        int c = col;
+        if(row < 3) r = 0;
+        else if(row< 6) r =3;
+        else r = 6;
+        if(col < 3) c = 0;
+        else if(col < 6) c = 3;
+        else c = 6;
+        for(int i = r ;i<r+3; i++)
+            for(int j = c ; j <c+3; j++)
+                if(board[i][j] == num) return false;
         return true;
     }
-    bool solve(vector<vector<char>>& board) {
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (board[i][j] == '.') {
-                    for (int num = 1; num <= 9; num++) {
-                        if (canPlace(board, i, j, (num + '0'))) {
-                            board[i][j] = num + '0';
+    bool solve(vector<vector<char>>& board){
+        for(int i = 0 ; i<board.size();i++){
+            for(int j= 0 ; j < board[0].size(); j++){
+                if(board[i][j] == '.'){
+                    for(int num = '1'; num <= '9'; num++){
+                        if(caniplace(board,i,j,num)){
+                            board[i][j] = num;
                             if(solve(board)) return true;
-                            else{
-                                board[i][j] = '.';
-                            }
+                            else board[i][j] = '.';
                         }
+                            
                     }
                     return false;
                 }
@@ -42,6 +35,6 @@ public:
         return true;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        if (solve(board))   return;
+        if(solve(board)) return;
     }
 };
