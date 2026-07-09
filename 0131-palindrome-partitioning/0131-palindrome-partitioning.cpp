4ade4f9d94dc11f390822646f1670447a749a18b
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-09 18:45:25 +0530

    Time: 55 ms (44.79%), Space: 79.7 MB (33.76%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    bool check(string s, int start , int end){
-        while(start < end){
-            if(s[start]!= s[end]) return false;
-            start++;end--;
-        }
-        return true;
-    }
-
     vector<vector<string>> ans;
-    void func(int index, vector<string>& arr, string s ){
+    void func(int index,vector<string>& v, string& s){
         if(index == s.size()){
-            ans.push_back(arr);
+            ans.push_back(v);
             return;
         }
-        for(int i=index; i<s.size(); i++){
-            if(check(s, index, i)){
-                string p =s.substr(index, i-index+1);
-                arr.push_back(p);
-                func(i+1,arr,s);
-                arr.pop_back();
+        for(int i = index ; i<s.size(); i++){
+            if(check(index, i, s)){
+                string k = s.substr(index,i-index+1);
+                v.push_back(k);
+                func(i+1,v,s);
+                v.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<string> arr;
-        func(0,arr,s);
+        vector<string> v;
+        func(0,v,s);
         return ans;
+    }   
+    bool check(int start, int end, string& s){
+        while(start <= end){
+            if(s[start++] != s[end--]) return false;
+        }
+        return true;
     }
 };
