74689569d7eeccccd8971532b5835188d9c22cac
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-23 14:59:59 +0530

    Time: 10 ms (26.93%), Space: 17.9 MB (5.28%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        vector<int> nse, pse, nge, pge;
-        stack<int> st;
+    void find(vector<int>& nums,vector<long long>& pse,vector<long long>& nse,vector<long long>& pge,vector<long long>& nge){
         int n = nums.size();
-        for(int  i = n-1; i>=0 ; i--){
-            while(!st.empty() && nums[st.top()]>= nums[i]) st.pop();
+        stack<int> st;
+        for(int i = n-1; i>= 0 ; i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]) st.pop();
             if(st.empty()) nse.push_back(n-i);
             else nse.push_back(st.top()-i);
             st.push(i);
         }
-        st = stack<int> ();
-        for(int  i = 0; i<n ; i++){
-            while(!st.empty() && nums[st.top()]> nums[i]) st.pop();
-            if(st.empty()) pse.push_back(i+1);
-            else pse.push_back(i-st.top());
-            st.push(i);
-        }
-        st = stack<int> ();
-        for(int i=n-1; i>= 0 ; i--){
-            while(!st.empty() && nums[st.top()]<= nums[i]) st.pop();
+        st =  stack<int> ();
+        for(int i = n-1; i>= 0 ; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]) st.pop();
             if(st.empty()) nge.push_back(n-i);
             else nge.push_back(st.top()-i);
             st.push(i);
         }
-        st = stack<int> ();
-        for(int  i = 0; i<n ; i++){
-            while(!st.empty() && nums[st.top()]< nums[i]) st.pop();
+        st =  stack<int> ();
+        for(int i = 0; i<n ; i++){
+            while(!st.empty() && nums[st.top()] < nums[i]) st.pop();
             if(st.empty()) pge.push_back(i+1);
             else pge.push_back(i-st.top());
             st.push(i);
         }
-        long long sum1 = 0 ,sum2 = 0;
-        for(int i=0;i<n;i++){
-            sum2 += (1LL*nums[i]*(pge[i]*nge[n-i-1]));
-            sum1 += (1LL*nums[i]*(nse[n-i-1]*pse[i]));
+        st =  stack<int> ();
+        for(int i = 0; i<n ; i++){
+            while(!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            if(st.empty()) pse.push_back(i+1);
+            else pse.push_back(i-st.top());
+            st.push(i);
+        }
+
+        return ;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        vector<long long> nse, pse,pge,nge;
+        find(nums,pse,nse,pge,nge);
+        long long sum1= 0, sum2 = 0;
+        for(int i=0; i<nums.size() ; i++){
+            sum1 += ((nums[i]*1LL)*nse[nums.size()-i-1]*pse[i]);
+            sum2 += ((nums[i]*1LL)*nge[nums.size()-i-1]*pge[i]);
+            
         }
         return sum2-sum1;
     }
