7d0f725044dd0bb0826b148d5e39f2f2948723bc
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-07-31 10:47:50 +0530

    Time: 3 ms (61.54%), Space: 10.4 MB (41.82%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
+    bool caniplace(int row, int col,int n, vector<string>& board){
+        int r , c;
+        for(int i=0;i<row;i++){
+            if(board[i][col] == 'Q') return false;
+        }
+        for(int i = 0; i<col ; i++){
+            if(board[row][i] == 'Q') return false;
+        }
+        r = row, c = col;
+        while(r<n && c<n && r>=0 && c>=0){
+            if(board[r--][c++] == 'Q') return false;
+        }
+        r = row, c = col;
+        while(r<n && c<n && r>=0 && c>=0){
+            if(board[r--][c--] == 'Q') return false;
+        }
+        return true;
+    }
     vector<vector<string>> ans;
-
-    void func(int row, vector<string>& board, int n,vector<int>& upper_left,vector<int>& upper_right,vector<int>& horizontal){
+    void f(int row,int n,vector<string>& board){
         if(row == n){
             ans.push_back(board);
             return;
-        }
-        for(int col = 0 ; col<n ;col++){
-            if(upper_left[row+col] == 0 && upper_right[n-1+row-col] == 0 && horizontal[col] == 0){
-                upper_left[row+col] = 1;
-                upper_right[n-1+row-col] = 1;
-                horizontal[col] = 1;
-                board[row][col] = 'Q';
-                func(row+1,board,n,upper_left,upper_right,horizontal);
-                board[row][col] = '.';
-                upper_left[row+col] = 0;
-                upper_right[n-1+row-col] = 0;
-                horizontal[col] = 0;
+        }else{
+            for(auto col = 0; col<n; col++){
+                if(caniplace(row,col,n,board)){
+                    // add to the board and recurse next row
+                    board[row][col] = 'Q';
+                    f(row+1,n,board);
+                    board[row][col] = '.';
+                    // remove from the board
+                }
             }
         }
-
-    }
+    }  
     vector<vector<string>> solveNQueens(int n) {
+        vector<string> board;
         string s;
-        for(int i = 0 ; i < n ; i++) s.push_back('.');
-        vector<string> board(n);
-        for(int i=0;i<n;i++) board[i] = s;
-
-        vector<int> upper_right(2*n-1);
-        vector<int> upper_left(2*n-1);
-        vector<int> horizontal(n);
-        func(0,board, n, upper_right,upper_left,horizontal);
-
+        for(auto x = 0; x<n; x++) s.push_back('.');
+        for(auto x = 0; x<n; x++) board.push_back(s);
+        int row  = 0;
+        f(row,n,board);
         return ans;
     }
 };
