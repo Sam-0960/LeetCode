65d9b5658fef2ce6cba65d3637faad00b5abc704
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-01 10:50:58 +0530

    Time: 1 ms (86.06%), Space: 153.8 MB (43.77%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -14,47 +14,34 @@ public:
     bool isEvenOddTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
-        int flag = 0;
-        vector<vector<int>> bfs;
+        int level = 0;
         while(!q.empty()){
             int n = q.size();
-            vector<int> lvl(n);
-            for(int i=0; i<n ; i++){
-                TreeNode* node = q.front();
+            int prev, curr;
+            for(int i=0; i<n ;i++){
+                TreeNode* node= q.front();
                 q.pop();
+                if( i == 0){
+                    prev = node->val;
+                    if(level%2 == 0 && prev%2 == 0) return false;
+                    else if(level%2 == 1 && prev%2 == 1) return false;
+                    // no comparision
+                }else{
+                    // comparion based on level
+                    curr = node->val;
+                    if(level%2 == 0){
+                        if(curr%2 == 0) return false;
+                        if(prev >= curr) return false;
+                    }else{
+                        if(curr%2 == 1) return false;
+                        if(prev <= curr) return false;
+                    }
+                    prev = curr;
+                }
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
-                lvl[i] = node->val;
             }
-            bfs.push_back(lvl);
-        }
-        int flg = 0;
-        for(int i = 0; i<bfs.size(); i++){
-            if(bfs[i].size() == 1){
-                if(flg == 0 ){ 
-                    if(bfs[i][0]%2 == 0)return false;
-                    flg = 1;
-                }
-                else{ 
-                    if(bfs[i][0]%2 == 1)return false;
-                    flg = 0;
-                }
-                continue;
-            }
-            else if(flg == 0){
-                for(int j=0; j<bfs[i].size()-1 ; j++){
-                    if(bfs[i][j]%2 == 0 || bfs[i][j+1]%2 == 0) return false;
-                    if(bfs[i][j] >= bfs[i][j+1]) return false;
-                }
-                flg = 1;
-            }else{
-                for(int j=bfs[i].size()-1; j>0; j--){
-                    if(bfs[i][j]%2 == 1 || bfs[i][j-1]%2 == 1) return false;
-                    if(bfs[i][j] >= bfs[i][j-1]) return false;
-                }
-                flg = 0;
-            }
-            
+            level++; 
         }
         return true;
     }
