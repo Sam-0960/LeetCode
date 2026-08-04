05fe32d8969b061654eb9577b24146f6d91a9037
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-04 21:37:14 +0530

    Time: 0 ms (100%), Space: 18.4 MB (31.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>& image, int sr, int sc, int color,int prev,vector<vector<int>>& vis){
+    void bfs(vector<vector<int>>& image, int sr, int sc, int color,int prev){
         queue<pair<int,int>> q;
         image[sr][sc] = color;
         q.push({sr,sc});
-        vis[sr][sc] = 1;
         while(!q.empty()){
             auto [dx,dy] = q.front();
             q.pop();
 
-            if(dx-1 >= 0 && image[dx-1][dy] == prev && !vis[dx-1][dy] == 1){
-                vis[dx-1][dy] = 1;
+            if(dx-1 >= 0 && image[dx-1][dy] == prev){
                 image[dx-1][dy] = color;
                 q.push({dx-1,dy});
             } 
-            if(dy-1 >= 0 && image[dx][dy-1] == prev && !vis[dx][dy-1] == 1){
-                vis[dx][dy-1] = 1;
+            if(dy-1 >= 0 && image[dx][dy-1] == prev){
+
                 image[dx][dy-1] = color;
                 q.push({dx,dy-1});
             } 
-            if(dx+1 <image.size() && image[dx+1][dy] == prev && !vis[dx+1][dy] == 1){
-                vis[dx+1][dy] = 1;
+            if(dx+1 <image.size() && image[dx+1][dy] == prev){
                 image[dx+1][dy] = color;
                 q.push({dx+1,dy});
             } 
-            if(dy+1 <image[0].size() && image[dx][dy+1] == prev && !vis[dx][dy+1] == 1){
-                vis[dx][dy+1] = 1;
+            if(dy+1 <image[0].size() && image[dx][dy+1] == prev){
                 image[dx][dy+1] = color;
                 q.push({dx,dy+1});
             } 
@@ -33,8 +29,8 @@ public:
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int prev = image[sr][sc];
-        vector<vector<int>> vis (image.size(),vector<int>(image[0].size(),0));
-        bfs(image,sr,sc,color,prev,vis);
+        if(prev == color) return image;
+        bfs(image,sr,sc,color,prev);
         return image;
     }
 };
