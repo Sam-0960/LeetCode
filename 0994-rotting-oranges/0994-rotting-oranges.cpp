823c4dcbf593969acb021f55a171b829abffca21
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-05 11:49:20 +0530

    Time: 0 ms (100%), Space: 16.7 MB (86.7%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.size()== 1 && grid[0].size() == 1 && grid[0][0] == 1) return -1;
         queue<pair<int,int>> q;
-        int rows = grid.size(), cols = grid[0].size();
+        int n = grid.size(); int m = grid[0].size();
         int total = 0;
-        for(int i=0; i<rows; i++){
-            for(int j = 0 ; j<cols; j++){
+        for(int i=0; i<n;i++){
+            for(int j = 0; j<m; j++){
                 if(grid[i][j] == 2) q.push({i,j});
                 if(grid[i][j] == 2 || grid[i][j] == 1) total++;
             }
         }
         if(total == 0) return 0;
+        if(q.empty()) return -1;
         int time = 0;
         while(!q.empty()){
-            int n = q.size();
-            total -= n;
+            int k = q.size();
             ++time;
-            for(int i=0;i<n;i++){
+            total -= k;
+            for(int i=0; i<k; i++){
                 auto [x,y] = q.front();
                 q.pop();
-                //up
-                if(x-1 >= 0 && grid[x-1][y] == 1){
+                if(x-1>=0 && grid[x-1][y] == 1){
                     grid[x-1][y] = 2;
                     q.push({x-1,y});
                 }
-                //down
-                if(x+1 <grid.size() && grid[x+1][y] == 1){
+                if(x+1<n && grid[x+1][y] == 1){
                     grid[x+1][y] = 2;
                     q.push({x+1,y});
                 }
-                //left
-                if(y-1 >= 0 && grid[x][y-1] == 1){
+                if(y-1>=0 && grid[x][y-1] == 1){
                     grid[x][y-1] = 2;
                     q.push({x,y-1});
                 }
-                //right
-                if(y+1 <grid[0].size() && grid[x][y+1] == 1){
+                if(y+1<m && grid[x][y+1] == 1){
                     grid[x][y+1] = 2;
                     q.push({x,y+1});
                 }
             }
         }
-        return (total == 0)?time-1:-1;
+        return (total == 0)? time-1:-1;
     }
 };
