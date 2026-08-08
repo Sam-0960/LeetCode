0ea8fbd720a9c3efa16eb75bb1c3eedd5bf75ad4
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-08 20:25:24 +0530

    Time: 93 ms (5.07%), Space: 73.5 MB (5.07%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& nodes,vector<int>& path,vector<int>& vis){
+        vis[node] = 1;
+        path[node] = 1;
+        for(auto x: adj[node]){
+            if(path[x]) return false;
+            if(!vis[x]&&!dfs(x,adj,nodes,path,vis)) return false;
+        }
+        path[node] = 0;
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+        vector<int> outdegree(n,0);
+        for(int i=0;i<n;i++){
+            vector<int> v;
+            for(int j =0; j<graph[i].size();j++){
+                v.push_back(graph[i][j]);
+            }
+            adj[i]= v;
+            outdegree[i]++;
+        }
+        vector<int> visited(n,0);
+        vector<int> path(n,0);
+        vector<int> nodes;
+        vector<int> ans;
+        for(auto x: outdegree) if(x == 0) nodes.push_back(x);
+        for(int i=0; i<outdegree.size() && outdegree[i] != 0; i++){
+            if(dfs(i,adj,nodes,path,visited)) ans.push_back(i);
+        }
+        for(auto x: ans) nodes.push_back(x);
+        return ans;
+    }
+};
