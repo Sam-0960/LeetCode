0449cae08da132ce95b45f3d50e9594e9e3a34da
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-09 11:52:59 +0530

    Time: 0 ms (100%), Space: 17.4 MB (79.22%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
-    bool dfs(int i,vector<vector<int>>& adj,vector<vector<int>>& graph,vector<int>& vis,vector<int>& color){
-        vis[i] = 1;
-        bool val = true;
-        for(auto x: adj[i]){
-            if(color[i] == 1){
-                if(!vis[x]){
-                    color[x] = 0;
-                    val = dfs(x,adj,graph,vis,color);
-                    if(val == false) return false;
-                }else{
-                    if(color[x] == 1) return false;
-                }
-            }else{
-                if(!vis[x]){
-                    color[x] = 1;
-                    val = dfs(x,adj,graph,vis,color);
-                    if(val == false) return false;
-                }else{
-                    if(color[x] == 0) return false;
-                }
-            }
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& color){
+        if(color[node] == -1) color[node] = 1;
+        for(auto x: adj[node]){
+            if(color[x] == color[node]) return false;
+            if(color[x] == -1){
+                color[x] = !color[node];
+                if(!dfs(x,adj,color)) return false;
+            } 
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<vector<int>> adj;vector<int> vis1(graph.size(),0);vector<int> color(graph.size(),-1);
-        for(int i=0;i<graph.size();i++){
-            vector<int> ans;
-            for(int j =0; j<graph[i].size();j++){
-                ans.push_back(graph[i][j]);
-            }
-            adj.push_back(ans);
-        }
-        // 1-> A 0-> B
-        color[0] = 1;bool val = true;
-        for(int i = 0; i<adj.size();i++){
-            if(!vis1[i]){
-                if(!dfs(i,adj,graph,vis1,color)) return false;
-            }
+        vector<int> color(graph.size(),-1);
+        for(int i=0; i<graph.size();i++){
+            if(color[i]==-1 && !dfs(i,graph,color)) return false;
         }
         return true;
     }
