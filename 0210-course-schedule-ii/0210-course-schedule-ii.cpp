5063ee68b508471611b88c91a9d5900d881bff76
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-09 12:15:25 +0530

    Time: 0 ms (100%), Space: 18.5 MB (50.52%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool dfs(int i,vector<vector<int>>& adj,vector<int>& path,vector<int>&vis,vector<int>&ans){
+    bool dfs(int i, vector<vector<int>>&adj,vector<int>& path,vector<int>& vis,vector<int>& ans){
         vis[i] = 1;
         path[i] = 1;
         for(auto x: adj[i]){
-            if(path[x]) return false;
+            if(path[x] == 1) return false;
             if(!vis[x] && !dfs(x,adj,path,vis,ans)) return false;
         }
         path[i] = 0;
@@ -12,13 +12,15 @@ public:
         return true;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
         vector<vector<int>> adj(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
+        for(int i=0; i<n ;i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
         vector<int> ans;
-        vector<int> path(numCourses,0);vector<int> vis(numCourses,0);
-        for(int i=0;i<numCourses;i++){
+        vector<int> path(numCourses,0);
+        vector<int> vis(numCourses,0);
+        for(int i =0 ; i<numCourses; i++){
             if(!vis[i] && !dfs(i,adj,path,vis,ans)) return {};
         }
         reverse(ans.begin(),ans.end());
