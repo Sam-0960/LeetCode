e318a150de0f641acd49ad38b31f0c74b818751d
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-11 07:45:13 +0530

    Time: 42 ms (87.82%), Space: 25.7 MB (34.04%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n = heights.size();int m = heights[0].size();
-        priority_queue<tuple<int, int, int>,vector<tuple<int, int, int>>,greater<tuple<int, int, int>>> pq;
-        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        vector<vector<int>> effort(n, vector<int>(m, INT_MAX));
-        effort[0][0] = 0;
-        pq.push({0, 0, 0});
-
-        while (!pq.empty()) {
-            auto [diff, x, y] = pq.top();
-            pq.pop();
-            for (auto [dx, dy] : directions) {
-                if (x + dx < 0 || y + dy < 0 ||x + dx >= n || y + dy >= m ) continue;
-                int edge = abs(heights[x+dx][y+dy] - heights[x][y]);
-                int newEffort = max(diff, edge);
-                if(effort[x+dx][y+dy] > newEffort){
-                    effort[x+dx][y+dy] = newEffort;
-                    pq.push({newEffort,x+dx,y+dy});
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<int>> eff(n,vector<int>(m,INT_MAX));
+        vector<pair<int,int>>directions = {{1,0},{0,1},{-1,0},{0,-1}};
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<tuple<int,int,int>>> pq;
+        pq.push({0,0,0});
+        eff[0][0] = 0;
+        while(!pq.empty()){
+            auto [effort,x,y] = pq.top();pq.pop();
+            if(x == n-1 && y == m-1) return effort;
+            for(auto [dx,dy] : directions ){
+                if(x+dx >= n || x+dx < 0 || y+dy >= m ||y+dy < 0)continue;
+                int weight = abs(heights[x+dx][y+dy] - heights[x][y]);
+                int maxeff = max(weight,effort);
+                if(eff[x+dx][y+dy] > maxeff){
+                    eff[x+dx][y+dy] = maxeff;
+                    pq.push({maxeff,x+dx,y+dy});
                 }
             }
         }
-
-        return effort[n-1][m-1];
+        return (eff[n-1][m-1] == INT_MAX)? -1: eff[n-1][m-1];
     }
 };
