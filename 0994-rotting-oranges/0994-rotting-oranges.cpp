0b3e7ff8eba8f0acdf14385cccd9e22fc6478044
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-13 09:17:57 +0530

    Time: 0 ms (100%), Space: 16.7 MB (86.63%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>> q;
-        int n = grid.size(); int m = grid[0].size();
-        int total = 0;
-        for(int i=0; i<n;i++){
-            for(int j = 0; j<m; j++){
+        queue<pair<int,int>> q; int total = 0;
+        int row = grid.size();int col = grid[0].size();
+        for(int i = 0; i<row;i++){
+            for(int j =0; j<col; j++){
                 if(grid[i][j] == 2) q.push({i,j});
-                if(grid[i][j] == 2 || grid[i][j] == 1) total++;
+                if(grid[i][j] ==1 || grid[i][j] == 2) total++;
             }
         }
         if(total == 0) return 0;
         if(q.empty()) return -1;
+        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
         int time = 0;
         while(!q.empty()){
-            int k = q.size();
-            ++time;
-            total -= k;
-            for(int i=0; i<k; i++){
-                auto [x,y] = q.front();
-                q.pop();
-                if(x-1>=0 && grid[x-1][y] == 1){
-                    grid[x-1][y] = 2;
-                    q.push({x-1,y});
-                }
-                if(x+1<n && grid[x+1][y] == 1){
-                    grid[x+1][y] = 2;
-                    q.push({x+1,y});
-                }
-                if(y-1>=0 && grid[x][y-1] == 1){
-                    grid[x][y-1] = 2;
-                    q.push({x,y-1});
-                }
-                if(y+1<m && grid[x][y+1] == 1){
-                    grid[x][y+1] = 2;
-                    q.push({x,y+1});
+            int n = q.size();
+            total = total -n;
+            for(int i=0; i<n; i++){
+                auto [x,y] = q.front(); q.pop();
+                for(auto [dx,dy] : dir){
+                    if(x+dx >= row || x+dx < 0 || y+dy >= col || y+dy <0 || grid[x+dx][y+dy] != 1)
+                        continue;
+                    grid[x+dx][y+dy] = 2;
+                    q.push({x+dx,y+dy});
                 }
             }
+            time++;
         }
-        return (total == 0)? time-1:-1;
+        if(total != 0) return -1;
+        return time-1;
     }
 };
