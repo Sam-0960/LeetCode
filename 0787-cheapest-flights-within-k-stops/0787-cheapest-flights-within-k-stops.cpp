4c3505460669d073ec772f819a6158d880a56889
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-16 12:16:24 +0530

    Time: 4 ms (45.87%), Space: 18.1 MB (50.64%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int,int>>> adj(n);
-        for(int i=0; i<flights.size(); i++){
+        for(int i=0; i<flights.size();i++){
             adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
         }
-        vector<int> distance(n,INT_MAX);
-        queue<tuple<int,int,int>> q;
+        vector<int>distance(n,INT_MAX);
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>> , greater<tuple<int,int,int>>> pq;
+        pq.push({0,src,0});
         distance[src] = 0;
-        q.push({0,src,0});
-        while(!q.empty()){
-            auto [stops,node,dist] = q.front();q.pop();
-            if(stops>k) continue;
-            for(auto [child,weight]: adj[node]){
-                if(distance[child] > dist+weight && stops <= k){
-                    distance[child] = dist+weight;
-                    q.push({stops+1,child,distance[child]});
+        int ans = INT_MAX;
+        while(!pq.empty()){
+            auto [stops,node,d] = pq.top();pq.pop();
+            if(node == dst && stops <= k+1) ans= min(ans,d);
+            if(stops >= k+1) continue;
+            for(auto [child,w] : adj[node]){
+                if(distance[child]>d+w){
+                    distance[child] = d+w;
+                    pq.push({stops+1,child,distance[child]});
                 }
             }
         }
-        return (distance[dst] == INT_MAX)? -1: distance[dst];
+        return (ans == INT_MAX)?-1:ans;
     }
 };
