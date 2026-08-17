6f2e8cd25cb84dff8bf1bba73a7505e899811e42
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-17 11:35:38 +0530

    Time: 799 ms (25.83%), Space: 11.8 MB (18.41%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    bool func(int row, int col,vector<vector<char>>& board, string word, int index){
-        if(index == word.size()) return true;
-        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[index])  return false;
+    bool func(vector<vector<char>>& board, string word,int i,int row,int col){
+        if(i == word.size()) return true;
+        if(row < 0 || row>= board.size() || col<0 || col>= board[0].size()|| board[row][col] != word[i])   return false;
         char el = board[row][col];
         board[row][col] = '#';
-
-        //go up
-        bool up = func(row-1,col,board,word ,index+1);
-        //go down
-        bool down = func(row+1,col,board,word ,index+1);
-        //go left
-        bool left = func(row,col-1,board,word ,index+1);
-        // go right
-        bool right = func(row,col+1,board,word ,index+1);
-
+        int u = func(board,word,i+1,row-1,col);
+        int l = func(board,word,i+1,row,col-1);
+        int r = func(board,word,i+1,row,col+1);
+        int d = func(board,word,i+1,row+1,col);
         board[row][col] = el;
-        return up||down||left||right;
+        return u||d||l||r;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        bool ans = false;
-        for(int i=0 ; i<board.size(); i++){
-            for(int j = 0 ; j<board[0].size(); j++){
-                if(func(i,j,board,word,0)) return true;
-            }
-        }
-        return ans;
+        int n = board.size(); int m = board[0].size();
+        for(int i=0;i<n;i++)
+            for(int j = 0;j<m;j++)
+                if(func(board,word,0,i,j)) return true;
+
+        return false;
     }
 };
