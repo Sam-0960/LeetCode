da3e3b0abde392cc74802f3bbdbf2813d96f3699
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-21 18:22:03 +0530

    Time: 68 ms (18.45%), Space: 94.2 MB (16.54%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int atmost(vector<int>& nums, int k){
-        int res = 0 ;
-        int l = 0 , r = 0;
-        int cnt = 0;
-        while(r<nums.size()){
-            if(nums[r] == 1) cnt++;
-            while(cnt > k){
-                if(nums[l] == 1) cnt--;
-                l++;
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] % 2 == 1) nums[i] = 1;
+            else nums[i] = 0;
+        }
+        int res = 0;
+        int sum = 0;
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        for(auto x: nums){
+            sum += x;
+            if(mp.find(sum-k) != mp.end()){
+                res+= mp[sum-k];
             }
-            res += r-l+1;
-            r++;
+            mp[sum]++;
         }
         return res;
-    }
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        for(int i = 0;i <nums.size(); i++){
-            if(nums[i]%2 == 0) nums[i] = 0;
-            else nums[i] = 1;
-        }
-        return atmost(nums,k) - atmost(nums,k-1);
     }
 };
