29578b797cebe8c365c710c741c196f0acdfe373
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-08-31 17:38:05 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n == 1) return 1;
-        if(n == 2) return 2;
-        pair<int,int> dp = {1,2};
-        for(int i=3 ;i<=n; i++){
-            int next = dp.first + dp.second;
-            dp.first = dp.second;
-            dp.second = next;
+        pair<int,int> p;
+        p.first = 1;
+        p.second = 2;
+        for(int i =3 ; i<= n ; i++){
+            int el = p.first + p.second;
+            p.first = p.second;
+            p.second = el;
         }
-        return dp.second;
+        return p.second;
     }
 };
