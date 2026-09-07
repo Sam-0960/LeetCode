41cb0ea5cede4f56bd75c8050d6b0cf8efbce63d
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-09-07 16:49:24 +0530

    Time: 14 ms (6.66%), Space: 18.1 MB (91.4%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    bool dfs(int i, vector<vector<int>>&adj,vector<int>& path,vector<int>& vis,vector<int>& ans){
-        vis[i] = 1;
-        path[i] = 1;
-        for(auto x: adj[i]){
-            if(path[x] == 1) return false;
-            if(!vis[x] && !dfs(x,adj,path,vis,ans)) return false;
-        }
-        path[i] = 0;
-        ans.push_back(i);
-        return true;
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = prerequisites.size();
-        vector<vector<int>> adj(numCourses);
-        for(int i=0; i<n ;i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-        }
         vector<int> ans;
-        vector<int> path(numCourses,0);
-        vector<int> vis(numCourses,0);
-        for(int i =0 ; i<numCourses; i++){
-            if(!vis[i] && !dfs(i,adj,path,vis,ans)) return {};
+        vector<int> indeg(numCourses,0);
+        vector<vector<int>> graph(numCourses);
+        for(int i=0; i<prerequisites.size();i++){
+            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indeg[prerequisites[i][0]]++;
         }
-        reverse(ans.begin(),ans.end());
+    
+        queue<int> q;
+        for(int i=0;i<indeg.size();i++){
+            if(indeg[i] == 0){
+                q.push(i);
+                ans.push_back(i);
+            } 
+        } 
+
+        while(!q.empty()){
+            auto node = q.front(); q.pop();
+            for(auto child : graph[node]){
+                indeg[child]--;
+                if(indeg[child] == 0){
+                    ans.push_back(child);
+                    q.push(child);
+                } 
+               
+            }
+        }
+        int a = accumulate(indeg.begin(),indeg.end(),0LL);
+        if(a>0) return {};
         return ans;
     }
 };
