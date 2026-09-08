187eb2d87fcd4e7e23cf26accaf499aa4f16c2b7
Samarth Prabhu <avsamarth.9a@gmail.com>
2026-09-08 08:45:38 +0530

    Time: 72 ms (90.26%), Space: 44.3 MB (70.09%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> dis(n+1,INT_MAX);
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(int i =0; i<times.size(); i++){
-            adj[times[i][0]].push_back({times[i][1],times[i][2]});
+        vector<vector<pair<int,int>>> graph(n+1);
+        for(int i = 0; i<times.size() ; i++){
+            int u = times[i][0];
+            int v = times[i][1];
+            int w = times[i][2];
+            graph[u].push_back({v,w});
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0,k});
+        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
+        vector<int> dis(n+1,INT_MAX);
         dis[k] = 0;
+        pq.push({0,k});
         while(!pq.empty()){
-            auto [d,node] = pq.top();
+            auto [w,node] = pq.top();
             pq.pop();
-            for(auto x:adj[node]){
-                if(dis[x.first] > x.second+dis[node]){
-                    dis[x.first] = x.second+dis[node];
-                    pq.push({dis[x.first],x.first});
+            if(dis[node] < w) continue;
+            for(auto [child,weight]: graph[node]){
+                if(dis[child] > dis[node] + weight){
+                    dis[child] = dis[node] + weight;
+                    pq.push({dis[child],child});
                 }
             }
         }
-        int ans = *max_element(dis.begin()+1,dis.end());
-        return (ans == INT_MAX)? -1: ans;
+        int res = *max_element(dis.begin()+1,dis.end());
+        return (res == INT_MAX)? -1 : res;
     }
 };
